24bd2da09b543041a31510a0cbaa7eba72eda25e
ranxfan <ranxfan@gmail.com>
2019-09-10 02:22:06 +0800

    feat：尝试部署

--- config/default.js
+++ config/default.js
@@ -3,6 +3,7 @@
 module.exports = {
 	port: parseInt(process.env.PORT, 10) || 8001,
 	url: 'mongodb://10.128.0.5:27017/elm',
+	// url: 'mongodb://192.168.88.28:37017/elm',
 	session: {
 		name: 'SID',
 		secret: 'SID',
